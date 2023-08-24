cb556f90205b3a02ae68692a1a299c5422b1db51
Leo Britten <leobritten@Leos-MBP-2.ht.home>
2023-08-23 22:06:39 -0500

    learned if stmts

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    
-}
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("please enter a number");
+        int num=scanner.nextInt();
+        System.out.println(num);
+
+        String positivestring="this is the positive string";
+        String negativestring="this is the negative string";
+        String zerostring="this is the zero string";
+        if(num >0){
+        System.out.println(positivestring);
+        }
+        else if(num ==0){System.out.println(zerostring);
+            }
+            else{
+            System.out.println(negativestring);
+        }
+
+//        string, int, bool, double
+//        string, int, bool, double
+
+
+
+
+}}
